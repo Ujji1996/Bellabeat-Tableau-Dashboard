3b47aa3d234ca9dc5264de4f69800219b9ce252b
Ujji1996 <ujwaldas1990@gmail.com>
2026-04-13 14:12:54 +0530

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,1 +1,5 @@
-
+-- STEP 1: Statistical Calculation
+SELECT 
+    AVG(SedentaryMinutes) / 60 AS avg_sedentary_hours,
+    AVG(VeryActiveMinutes + FairlyActiveMinutes) AS avg_high_intensity_minutes
+FROM `nifty-yeti-492810-d4.bellabeat_data.dailyActivity_merged`;
